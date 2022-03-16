33a9c764caa274f371bfa629c09511d4f74221a8
Paschal Ramdan <dane.ramdan@gmail.com>
2022-03-16 00:09:14 -0400

    commit tree 64ca7d2267eb1e6b7d997ff267d2b0c2789a5dfa

---
+++ index.js
@@ -0,0 +1,25 @@
+const divide = function(num1, num2){
+    return 2000 / 100
+}
+
+let square = x => {
+    return x * 2
+}
+
+let add = (a, b) => {
+    return a + b
+}
+
+
+
+
+// it("has an arrow function called add", () => {
+//     expect(add).to.exist
+//   })
+
+//   it("add arrow function takes two parameters and adds them together", () => {
+//     let a = 3
+//     let b = 4
+//     expect(add(a,b)).to.equal(a+b)
+//   })
+// })
